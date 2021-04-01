de1e180c48b202fef5ad683fde7509329102c016
Josh <blkjosh923@gmail.com>
2021-03-31 21:24:56 -0500

    *User Authentication added

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/AuthenticationFilter.java
@@ -0,0 +1,61 @@
+package org.launchcode.javawebdevtechjobsauthentication;
+
+import org.launchcode.javawebdevtechjobsauthentication.controllers.AuthenticationController;
+import org.launchcode.javawebdevtechjobsauthentication.models.User;
+import org.launchcode.javawebdevtechjobsauthentication.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    //"whitelist pages that any user can access such as logging in/out
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
+
+    //"checks compares parameters with items listed in whitelist"
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    //"Pre handle"
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws IOException {
+
+        // Check for whitelisted pages within pre handle if returns true it will proceed
+        if (isWhitelisted(request.getRequestURI())) {
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        // The user is logged in
+        if (user != null) {
+            return true;
+        }
+
+        // The user is NOT logged in
+        response.sendRedirect("/login");
+        return false;
+    }
+}
